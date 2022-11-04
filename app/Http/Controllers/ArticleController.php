e2bbb5523bf7629fe45eb0bf731939943ad10cf5
FLEURY <angekoffi160@gmail.com>
2022-11-04 18:55:59 +0000

    les routes

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view("article");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'libelle'=> 'required|max:25',
+            'prix_vente'=> 'required|max:25',
+            'volume'=> 'required|max:25',
+            ]
+        );
+
+        $article= new Article();
+        $article->libelle = $request->libelle;
+        $article->prix_vente = $request->prix_vente;
+        $article->volume = $request->volume;
+        $article->save();
+
+        if( $article){
+            $msg = "L'article $request->libelle a été ajouté avec succès!";
+            $etat = 'success';
+        }else{
+            $msg = 'Erreur! Veuillez réesayer SVP et verifier votre connexion';
+            $etat = 'warning';
+        }
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/FournisseurController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fournisseur;
+use Illuminate\Http\Request;
+
+class FournisseurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom'=> 'required|max:25',
+            'type_marchandise'=> 'required|max:25',
+            'adresse'=> 'required|max:25',
+            'localisation'=> 'required|max:25',
+            ]
+        );
+        $fournisseur = new Fournisseur();
+        $fournisseur->nom = $request ->nom;
+        $fournisseur->type_marchandise = $request ->type_marchandise;
+        $fournisseur->adresse= $request ->adresse;
+        $fournisseur->localisation = $request ->localisation;
+        $fournisseur->save();
+        if( $fournisseur){
+            $msg = "Le fournisseur $request->libelle a été ajouté avec succès!";
+            $etat = 'success';
+        }else{
+            $msg = 'Erreur! Veuillez réesayer SVP et verifier votre connexion';
+            $etat = 'warning';
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
