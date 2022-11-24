a098b11a5e1e03ecf592d1c2c7c5e59d76b5d074
KamadoSama1 <tahoarthur@gmail.com>
2022-11-24 19:08:53 +0000

    front-end

--- resources/views/approvionnement/create.blade.php
+++ resources/views/approvionnement/create.blade.php
@@ -4,23 +4,26 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="{{url('assets/css/approvisionnement.css')}}" />
+        <link rel="stylesheet" href="{{url('assets/css/formulaire.css')}}" />
+        <link rel="stylesheet" href="{{url('assets/css/menu.css')}}" />
+        <link rel="stylesheet" href="{{url('assets/css/index.css')}}" />
+
         <title>approvisionnement</title>
     </head>
     <body>
         <header>
-            
+
         </header>
         <main>
             <div class="videHaut"></div>
             <div class="menu">
                 <h1>Approvisionner les stocks</h1>
-                
+
                 <!-- Formulaires d'ajout des approvisionnements -->
                 <form action="" class="form">
                     <div id="appro" class="image">
                         <img
-                        src="boxes-amico.svg"
+                        src="{{asset("assets/css/boxes-amico.svg") }}"
                         alt="un"
                         height="100%"
                         width="100%"
@@ -36,7 +39,7 @@
                             <!-- <label for="quant">Quantité </label><br> -->
                             <input type="number" value="{{old('volume')}}"   name="volume" id="quant" placeholder="Quantité " />
                         </p>
-                        
+
                         <p>
                             <!-- <label for="visa_vend">Visa vendeur</label><br> -->
                             <input type="text" id="visa_vend" placeholder="Visa vendeur" />
@@ -47,20 +50,20 @@
                         </p>
                         <p>
                             <label for="obser">Observations</label><br />
-                            
+
                             <textarea name="" id="" cols="30" rows="10" id="obser"></textarea>
                         </p>
-                        
+
                         <input type="submit" value="Enregistrer">
-                        
+
                     </div>
-                    
+
                 </form>
-                
+
                 <div class="blabla">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus reiciendis vitae neque itaque! Distinctio, id nostrum fugiat tenetur facere enim, cum iste esse provident ullam consectetur, sed nemo voluptatem veniam.
                 </div>
-                
+
             </div>
         </main>
     </body>
